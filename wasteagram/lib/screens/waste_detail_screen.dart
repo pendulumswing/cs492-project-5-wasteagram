a90554586b6b1b0e1b03a332d7d571ff4a4a9b71
pendulumswing <jason@pendulumswingmedia.com>
2022-08-04 19:45:03 -0700

    Update waste_detail_screen.dart

--- wasteagram/lib/screens/waste_detail_screen.dart
+++ wasteagram/lib/screens/waste_detail_screen.dart
@@ -1,27 +1,21 @@
-// TODO:
 import 'package:flutter/material.dart';
 
-class WasteDetailScreen extends StatefulWidget {
+class WasteDetailScreen extends StatelessWidget {
   static const route = '/waste_detail';
   final String title = 'Waste Detail';
   const WasteDetailScreen({Key? key}) : super(key: key);
 
   @override
-  State<WasteDetailScreen> createState() => _WasteDetailScreenState();
-}
-
-class _WasteDetailScreenState extends State<WasteDetailScreen> {
-  @override
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Wasteagram'),
       ),
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: Text('${widget.title} Screen',
-            style: Theme.of(context).textTheme.headline4),
+        child: Column(children: [
+          Text('$title Screen', style: Theme.of(context).textTheme.headline4),
+        ]),
       ),
     );
   }
