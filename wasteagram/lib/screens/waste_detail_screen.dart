1957927e900f377e14831cbf617533e05ebde15f
pendulumswing <jason@pendulumswingmedia.com>
2022-08-05 17:47:01 -0700

    Update waste_detail_screen.dart

--- wasteagram/lib/screens/waste_detail_screen.dart
+++ wasteagram/lib/screens/waste_detail_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import '../models/food_waste_post.dart';
 
 class WasteDetailScreen extends StatelessWidget {
   static const route = '/waste_detail';
   final String title = 'Waste Detail';
-  const WasteDetailScreen({Key? key}) : super(key: key);
+  final FoodWastePost post;
+  const WasteDetailScreen({Key? key, required this.post}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +14,37 @@ class WasteDetailScreen extends StatelessWidget {
         title: const Text('Wasteagram'),
       ),
       body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(children: [
-          Text('$title Screen', style: Theme.of(context).textTheme.headline4),
-        ]),
+        padding: const EdgeInsets.all(0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(post.getFormattedDateShort,
+                style: Theme.of(context).textTheme.headline4),
+            Image.network(
+              post.imageUrl,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress == null) {
+                  return child;
+                } else {
+                  return const SizedBox(
+                    width: 800,
+                    height: 300,
+                    child: Center(child: CircularProgressIndicator()),
+                    // child: Center(child: LinearProgressIndicator()),
+                  );
+                }
+              },
+            ),
+            Text(post.quantity.toString(),
+                style: Theme.of(context).textTheme.headline4),
+            Text(
+              'Location: \n(${post.latitude}, ${post.longitude})',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        ),
       ),
     );
   }
