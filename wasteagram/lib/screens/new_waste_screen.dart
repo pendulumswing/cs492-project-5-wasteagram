9ee4061f65d1859d4ebaf897bca7274f3d2a8a23
pendulumswing <jason@pendulumswingmedia.com>
2022-08-05 17:57:36 -0700

    Update new_waste_screen.dart

--- wasteagram/lib/screens/new_waste_screen.dart
+++ wasteagram/lib/screens/new_waste_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:wasteagram/services/photo_storage_service.dart';
 import '../widgets/wasted_food_form.dart';
 
